852f25b8e1d3b29300fbb8977407fd6fad3a40b0
Griffin Stone <gstone@pdx.edu>
2017-06-02 17:01:28 -0700

    Initial commit

---
+++ bot.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+cd `dirname $0`
+
+BOT_NICK="echobot"
+KEY="$(cat ./config.txt)"
+
+nanos=1000000000
+interval=$(( $nanos * 50 / 100 ))
+declare -i prevdate
+prevdate=0
+
+function send {
+    while read -r line; do
+      newdate=`date +%s%N`
+      if [ $prevdate -gt $newdate ]; then sleep `bc -l <<< "($prevdate - $newdate) / $nanos"`; newdate=`date +%s%N`; fi
+      prevdate=$newdate+$interval
+      echo "-> $1"
+      echo "$line" >> ${BOT_NICK}.io
+    done <<< "$1"
+}
+
+nickCheckInterval=600 #seconds
+nextNickCheck=$((`date +%s` + $nickCheckInterval))
+allNicks=""
+export NICKS_SED=""
+
+rm ${BOT_NICK}.io
+mkfifo ${BOT_NICK}.io
+tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while [ -z "$started" ] || read -r irc ; do
+    if [ -z "$started" ] ; then
+        send "NICK $BOT_NICK" 
+        send "USER 0 0 0 :$BOT_NICK"
+        send "JOIN #robots $KEY"
+        started="yes"
+        read -r irc
+    fi
+    if $(<<<"$irc" cut -d ' ' -f 1 | grep -qP "PING") ; then
+        send "PONG"
+        date=`date +%s`
+        if [ $nextNickCheck -lt $date ]; then allNicks=""; send "NAMES #robots"; nextNickCheck=$(($date + $nickCheckInterval)); fi 
+    elif $(<<<"$irc" cut -d ' ' -f 2 | grep -qP "PRIVMSG") ; then 
+#:nick!user@host.cat.pdx.edu PRIVMSG #bots :This is what an IRC protocol PRIVMSG looks like!
+        nick="$(<<<"$irc" cut -d ':' -f 2- | cut -d '!' -f 1)"
+        chan="$(<<<"$irc" cut -d ' ' -f 3)"
+        if [ "$chan" = "$BOT_NICK" ] ; then chan="$nick" ; fi 
+        msg="$(<<<"$irc" cut -d ' ' -f 4- | cut -c 2- | tr -d "\r\n")"
+        echo "$(date) | $chan <$nick>: $msg"
+        var="$(echo "$nick" "$chan" "$BOT_NICK" "$msg" | ./commands.sh)"
+        if [ ! -z "$var" ] ; then
+            send "$var"
+        fi
+    elif $(<<<"$irc" cut -d ":" -f 2 | grep -q "353") ; then
+#:iss.cat.pdx.edu 353 irctest @ #robots :irctest uelen28 Apollo el_seano +zb bloc sten1ai spooklozgne +videocats gilben mensi Nascha manta +mathbot uelen...
+      allNicks="$allNicks $(echo -n "$irc" | cut -d ':' -f 3- | tr -d "\r\n+@")"
+    elif $(<<<"$irc" cut -d ":" -f 2 | grep -q "366") ; then
+#End of nicks list
+      echo "Got nicks $allNicks"
+      NICKS_SED=$(for nick in $allNicks; do echo "s/$nick/$(<<<$nick tr 'aeiostl' '43105+|')/g"; done)
+      #echo "Nicks sed: `<<<"$NICKS_SED" tr '\n' ';'`"
+    
+    fi
+done
---
+++ commands.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+read -r nick chan BOT_NICK msg 
+function count {
+  pgrep -c -u griffins sleepecho.sh
+}
+
+target_chan="#robots"
+odds=$((16 + 4 * `count`))
+sleep_scale=1800 # 30 min
+sleep_min=1 #30 min
+sleep_add=40 #20 hr
+min_len=24
+max_len=200
+ignore='^(!|%|\+|m[aeiou]th|infoobot|remind me|zb )'
+ignorenick="^(zb|icinga|jackson|notify)"
+interesting="(`tr '\n' '|' < interesting.txt | sed 's/|$//'`)"
+caps="[A-Z]{4}"
+
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#BOT_NICK="$(grep -P "BOT_NICK=.*" ${DIR}/bot.sh | cut -d '=' -f 2- | tr -d '"')"
+
+function debug {
+  echo "$@" 1>&2
+}
+
+function say { 
+  echo "PRIVMSG $chan :$1" > ${BOT_NICK}.io
+}
+
+priv=1
+
+if [ "$chan" = "$BOT_NICK" ] ; then chan="$nick";priv=0 ; fi
+
+run() {
+  output=`<<<$1 sed "$NICKS_SED"`
+  time=$(( ( $RANDOM % $sleep_add + $sleep_min ) * $sleep_scale ))
+  debug "will echo in $time s: $output"
+  if grep -qiP "\001ACTION" <<<"$1"; then
+    output="$output"
+  else
+    output="...$output"
+  fi
+  ./sleepecho.sh $time "PRIVMSG $target_chan :$output" > ${BOT_NICK}.io &
+}
+
+name="echo(bot)?"
+
+
+runCmd() {
+  case "`<<<"$1" tr '[:upper:]' '[:lower:]'`" in
+    ""|help) say "Echobot." ;;
+    ignore) say "I ignore messages with /$ignore/ and nicks with /$ignorenick/" ;;
+    waiting) say `count` ;;
+    clear) say "Clearing `count`"; pkill -u griffins sleepecho.sh ;;
+    restart) screen -r echobot -X kill ;;
+    *) say "$nick: Unrecognized command $cmd" ;;
+  esac
+}
+
+if grep -qiP "^${name}_[a-zA-Z]" <<<"$msg"; then
+  runCmd `cut -d '_' -f 2- <<<"$msg" | cut -d ' ' -f 1`
+
+elif grep -qiP "^!${name} " <<<"$msg"; then
+  runCmd "`cut -d ' ' -f 2- <<<"$msg"`"
+
+elif grep -qiP "^!${name}" <<<"$msg"; then
+  runCmd ""
+
+else
+  summon=0;
+  if grep -qiP "^${name} " <<<"$msg"; then 
+    debug "Summoned"
+    summon=1
+    msg=`cut -d ' ' -f 2- <<<"$msg"`
+    odds=$(($odds / 4))
+  fi
+  if [ $summon -eq 1 ] || [ ${#msg} -ge $min_len -a ${#msg} -le $max_len ] && grep -qiP -v "$ignore" <<<"$msg" && grep -qiP -v "$ignorenick" <<<"$nick"; then
+    debug "valid: $msg"
+    if grep -qP "$caps" <<<"$msg" || grep -qiP "$interesting" <<<"$msg"; then
+      debug "Interesting `grep -iP --color=always "$interesting" <<<"$msg"`"
+      odds=$(($odds / 2))
+    fi
+    if [ $(( $RANDOM % $odds)) -eq 0 ]; then
+      run "$msg"
+    fi
+  fi
+fi
