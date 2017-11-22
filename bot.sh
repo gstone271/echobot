fa752069b3a53da6a97ed916987ac9f14cbbff39
Griffin Stone <gstone@pdx.edu>
2017-11-22 12:51:14 -0800

    Remove unneccessary $() and -P from if

--- bot.sh
+++ bot.sh
@@ -28,19 +28,19 @@ export NICKS_SED=""
 
 rm ${BOT_NICK}.io
 mkfifo ${BOT_NICK}.io
-tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while [ -z "$started" ] || read -r irc ; do
-    if [ -z "$started" ] ; then
+tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while [[ -z "$started" ]] || read -r irc ; do
+    if [[ -z "$started" ]] ; then
         send "NICK $BOT_NICK" 
         send "USER 0 0 0 :$BOT_NICK"
         send "JOIN #robots $KEY"
         started="yes"
         read -r irc
     fi
-    if $(<<<"$irc" cut -d ' ' -f 1 | grep -qP "PING") ; then
+    if <<<"$irc" cut -d ' ' -f 1 | grep -q "PING" ; then
         send "PONG"
         date=`date +%s`
         if [ $nextNickCheck -lt $date ]; then allNicks=""; send "NAMES #robots"; nextNickCheck=$(($date + $nickCheckInterval)); fi 
-    elif $(<<<"$irc" cut -d ' ' -f 2 | grep -qP "PRIVMSG") ; then 
+    elif <<<"$irc" cut -d ' ' -f 2 | grep -q "PRIVMSG" ; then 
 #:nick!user@host.cat.pdx.edu PRIVMSG #bots :This is what an IRC protocol PRIVMSG looks like!
         nick="$(<<<"$irc" cut -d ':' -f 2- | cut -d '!' -f 1)"
         chan="$(<<<"$irc" cut -d ' ' -f 3)"
@@ -51,10 +51,10 @@ tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while 
         if [ ! -z "$var" ] ; then
             send "$var"
         fi
-    elif $(<<<"$irc" cut -d ":" -f 2 | grep -q "353") ; then
+    elif <<<"$irc" cut -d ":" -f 2 | grep -q "353" ; then
 #:iss.cat.pdx.edu 353 irctest @ #robots :irctest uelen28 Apollo el_seano +zb bloc sten1ai spooklozgne +videocats gilben mensi Nascha manta +mathbot uelen...
       allNicks="$allNicks $(echo -n "$irc" | cut -d ':' -f 3- | tr -d "\r\n+@")"
-    elif $(<<<"$irc" cut -d ":" -f 2 | grep -q "366") ; then
+    elif <<<"$irc" cut -d ":" -f 2 | grep -q "366" ; then
 #End of nicks list
       echo "Got nicks $allNicks"
       NICKS_SED=$(for nick in $allNicks; do echo "s/$nick/$(<<<$nick tr 'aeiostl' '43105+|')/gI"; done)
